6f18ae083a8b8e0a3ce46a32547dc24740966143
jiangpeiquan <jiangpeiquan1991@163.com>
2020-07-07 22:11:13 +0800

    新增方法

---
+++ stringUtil.go
@@ -0,0 +1,13 @@
+// package util
+
+// import "strings"
+
+// // IsBlank 字符串是否为空
+// func IsBlank(str string) bool {
+// 	return strings.TrimSpace(str) == ""
+// }
+
+// // IsNotBlank 字符串是否不为空
+// func IsNotBlank(str string) bool {
+// 	return !IsBlank(str)
+// }
