c3996d298d3b36025074cb668d10838763b40b87
jiangpeiquan <jiangpeiquan1991@163.com>
2020-07-07 22:12:32 +0800

    1

--- stringUtil.go
+++ stringUtil.go
@@ -1,13 +1,13 @@
-// package util
+package util
 
-// import "strings"
+import "strings"
 
-// // IsBlank 字符串是否为空
-// func IsBlank(str string) bool {
-// 	return strings.TrimSpace(str) == ""
-// }
+// IsBlank 字符串是否为空
+func IsBlank(str string) bool {
+	return strings.TrimSpace(str) == ""
+}
 
-// // IsNotBlank 字符串是否不为空
-// func IsNotBlank(str string) bool {
-// 	return !IsBlank(str)
-// }
+// IsNotBlank 字符串是否不为空
+func IsNotBlank(str string) bool {
+	return !IsBlank(str)
+}
